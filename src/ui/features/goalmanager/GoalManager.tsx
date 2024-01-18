f88d6e40460cf9bc231448e41962baff59472ac6
Manuel Dinis Júnior <91975829+manueldinisjunior@users.noreply.github.com>
2024-01-18 22:49:53 +0200

    Goal Manager

--- src/ui/features/goalmanager/GoalManager.tsx
+++ src/ui/features/goalmanager/GoalManager.tsx
@@ -251,3 +251,26 @@ export function GoalManager(props: Props) {
     {/* ... */}
   )
 }
+
+// GoalManager.tsx
+
+import { updateGoal as updateGoalApi } from '../../../api/lib'
+// ...
+
+export function GoalManager(props: Props) {
+  // ...
+
+  const pickEmojiOnClick = () => (emoji: BaseEmoji, event: MouseEvent) => {
+    // ...
+
+    const updatedGoal: Goal = {
+      ...props.goal,
+      icon: emoji.native ?? props.goal.icon,
+      name: name ?? props.goal.name,
+      targetDate: targetDate ?? props.goal.targetDate,
+      targetAmount: targetAmount ?? props.goal.targetAmount,
+    }
+
+    updateGoalApi(props.goal.id, updatedGoal)
+  }
+}

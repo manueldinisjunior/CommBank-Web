6d5ed7c571ef7a4c34ac07bdd6b2e21efa6daa00
Manuel Dinis Júnior <91975829+manueldinisjunior@users.noreply.github.com>
2024-01-18 22:40:17 +0200

    Update GoalManager.tsx

--- src/ui/features/goalmanager/GoalManager.tsx
+++ src/ui/features/goalmanager/GoalManager.tsx
@@ -182,3 +182,72 @@ const StringInput = styled.input`
 const Value = styled.div`
   margin-left: 2rem;
 `
+
+// GoalManager.tsx
+
+export function GoalManager(props: Props) {
+  // ...
+
+  const [icon, setIcon] = useState<string | null>(null)
+
+  useEffect(() => {
+    setIcon(props.goal.icon)
+  }, [props.goal.id, props.goal.icon])
+
+  const hasIcon = () => icon != null
+
+  const addIconOnClick = (event: React.MouseEvent) => {
+    event.stopPropagation()
+    setEmojiPickerIsOpen(true)
+  }
+
+  return (
+    {/* ... */}
+    <AddIconButtonContainer hasIcon={hasIcon()}>
+      <TransparentButton onClick={addIconOnClick}>
+        <FontAwesomeIcon icon={faSmile} size="2x" />
+        <AddIconButtonText>Add icon</AddIconButtonText>
+      </TransparentButton>
+    </AddIconButtonContainer>
+    {/* ... */}
+  )
+}
+
+// GoalIcon.tsx
+
+const Icon = styled.h1`
+  font-size: 6rem;
+  cursor: pointer;
+`
+
+export default function GoalIcon(props: Props) {
+  return (
+    <TransparentButton onClick={props.onClick}>
+      <Icon>{props.icon}</Icon>
+    </TransparentButton>
+  )
+}
+
+// GoalManager.tsx
+
+type GoalIconContainerProps = { shouldShow: boolean }
+
+const GoalIconContainer = styled.div<GoalIconContainerProps>`
+  display: ${(props) => (props.shouldShow ? 'flex' : 'none')};
+`
+
+export function GoalManager(props: Props) {
+  // ...
+
+  const hasIcon = () => icon != null
+
+  const goal = useAppSelector(selectGoalsMap)[props.goal.id]
+
+  return (
+    {/* ... */}
+    <GoalIconContainer shouldShow={hasIcon()}>
+      <GoalIcon icon={goal.icon} onClick={addIconOnClick} />
+    </GoalIconContainer>
+    {/* ... */}
+  )
+}
